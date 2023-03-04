7baf50d16c23755c179936a8fd7868644e3bb0bc
nrb04 <nredoyan@gmail.com>
2023-03-04 09:53:39 +0600

    , solve

--- js/app.js
+++ js/app.js
@@ -29,7 +29,7 @@ const displayCard = tools =>{
             <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
             
               <ol class="list-decimal">
-             ${tool.features.map ((fdata) => "<li>" + fdata +". </li>") }
+             ${tool.features.map ((fdata) => "<li>" + fdata +". </li>").join("")}
                </ol>
             </p>
 
